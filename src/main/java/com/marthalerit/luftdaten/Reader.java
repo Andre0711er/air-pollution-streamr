83f5b95c3e6ec961a0f9be92baa7c74c39ed9966
André Marthaler <am@marthaler-it.com>
2021-06-21 16:20:59 +0200

    add first shot to publish data to stream

--- src/main/java/com/marthalerit/luftdaten/Reader.java
+++ src/main/java/com/marthalerit/luftdaten/Reader.java
@@ -1,9 +1,14 @@
 package com.marthalerit.luftdaten;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonParser;
 import com.marthalerit.helper.StringHelper;
 import com.marthalerit.redis.Redis;
+import com.marthalerit.skeleton.Config;
 import com.marthalerit.skeleton.Skeleton;
+import com.streamr.client.StreamrClient;
+import com.streamr.client.authentication.EthereumAuthenticationMethod;
+import com.streamr.client.rest.Stream;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
@@ -18,6 +23,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
+import java.util.Map;
 import java.util.Optional;
 
 public class Reader implements Job {
@@ -35,43 +41,15 @@ public class Reader implements Job {
     }
   }
 
-//  /**
-//   * @see <a href="https://github.com/opendata-stuttgart/meta/wiki/APIs">https://github.com/opendata-stuttgart/meta/wiki/APIs</a>
-//   * https://data.sensor.community/static/v2/data.json
-//   */
-//  public void run() throws URISyntaxException, IOException, InterruptedException {
-//    try (Jedis redis = Redis.connect()) {
-//      final HttpClient client = HttpClient.newHttpClient();
-//
-//      final HttpRequest headRequest = HttpRequest.newBuilder().uri(new URI(endpoint))
-//          .timeout(Duration.ofSeconds(5))
-//          .method("HEAD", HttpRequest.BodyPublishers.noBody())
-//          .build();
-//
-//      final HttpHeaders headers = client.send(headRequest, HttpResponse.BodyHandlers.discarding()).headers();
-//      final String lastModifiedKey = "last-modified";
-//      Optional<String> lastModified = headers.firstValue(lastModifiedKey);
-//      if (lastModified.isPresent()) {
-//        final String lastModifiedStored = redis.get(lastModifiedKey);
-//        final String lastModifiedDetected = lastModified.get();
-//        if (!lastModifiedStored.equals(lastModifiedDetected)) {
-//          logger.debug("Execute Request; get new data");
-//          executeRequest();
-//          redis.set(lastModifiedKey, lastModifiedDetected);
-//        }
-//      }
-//    }
-//  }
-
   public void run() throws URISyntaxException, IOException, InterruptedException {
     try (Jedis redis = Redis.connect()) {
       final String lastModifiedKey = "last-modified";
 
       final HttpClient client = HttpClient.newHttpClient();
       final HttpRequest.Builder request = HttpRequest.newBuilder()
-          .uri(new URI(endpoint))
-          .timeout(Duration.ofSeconds(10))
-          .GET();
+              .uri(new URI(endpoint))
+              .timeout(Duration.ofSeconds(10))
+              .GET();
       final String lastModifiedStored = redis.get(lastModifiedKey);
       if (lastModifiedStored != null) {
         request.header("If-Modified-Since", lastModifiedStored);
@@ -94,12 +72,13 @@ public class Reader implements Job {
               final String key = StringHelper.getHash(value);
               if (redis.get(key) == null) {
                 // send into streamr queue
+                streamr(value);
                 // add to redis set
                 final SetParams params = new SetParams().ex(600L);
                 redis.set(key, value, params);
                 logger.debug("Store key/value in store; key: {}", key);
               }
-            } catch (NoSuchAlgorithmException e) {
+            } catch (NoSuchAlgorithmException | IOException e) {
               logger.error(e.getMessage());
             }
           });
@@ -107,6 +86,16 @@ public class Reader implements Job {
         }
       }
     }
+  }
+
+  public void streamr(final String message) throws IOException {
+    final Map map = new Gson().fromJson(message, Map.class);
+
+    final String privateKey = Config.get("streamr.key");
+    final StreamrClient client = new StreamrClient(new EthereumAuthenticationMethod(privateKey));
+    final Stream stream = client.getStream(Config.get("streamr.stream"));
+
+    client.publish(stream, map);
   }
 
   public static void main(String... args) throws URISyntaxException, IOException, InterruptedException {
---
+++ src/test/java/com/marthalerit/luftdaten/ReaderTest.java
@@ -0,0 +1,4 @@
+package com.marthalerit.luftdaten;
+
+public class ReaderTest {
+}
